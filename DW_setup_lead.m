05108a2b0e8c156aa0bc8998cbb6a37a9a0eee30
Dengyu Wang <Dengyu@Guests-iMac.local>
2018-04-20 15:31:01 -0400

    make local lead_dbs connected with remote github lead_dbs

---
+++ DW_setup_lead.m
@@ -0,0 +1,7 @@
+addpath(genpath('/Users/Dengyu/Downloads/spm12'));
+addpath(genpath('/Users/Dengyu/Downloads/lead'));
+
+
+rmpath(genpath('/Users/Dengyu/Downloads/spm12'));
+rmpath(genpath('/Users/Dengyu/Downloads/lead'));
+
