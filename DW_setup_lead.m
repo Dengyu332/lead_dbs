49743f1ee54b47be6fb8cdfbcd92785ce857808b
Wang <DEW93@pitt.edu>
2018-08-21 11:33:34 -0400

    DW_setup_lead

--- DW_setup_lead.m
+++ DW_setup_lead.m
@@ -1,7 +1,20 @@
+% on mac
 addpath(genpath('/Users/Dengyu/Downloads/spm12'));
 addpath(genpath('/Users/Dengyu/Downloads/lead'));
-
-
+%on mac
 rmpath(genpath('/Users/Dengyu/Downloads/spm12'));
 rmpath(genpath('/Users/Dengyu/Downloads/lead'));
+
+%on windows server
+addpath(genpath('E:\MATLAB\spm12'));
+addpath(genpath('E:\MATLAB\lead_v2.1.5')); % v2.1.5
+addpath(genpath('E:\MATLAB\lead')); % v2.1.0
+addpath(genpath('E:\MATLAB\leaddbs-master')); % old version of lead_dbs
+
+%on windows server
+rmpath(genpath('E:\MATLAB\spm12'));
+rmpath(genpath('E:\MATLAB\lead_v2.1.5')); % v2.1.5
+rmpath(genpath('E:\MATLAB\lead')); % v2.1.0
+rmpath(genpath('E:\MATLAB\leaddbs-master')); % old version of lead_dbs
+
 
